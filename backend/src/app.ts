006d1ad1bc8f7dd129ff7c55fce8d6569973b86d
xaltyPasta <rajkumarravi9470@gmail.com>
2026-03-07 00:30:32 +0530

    error log

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,21 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get("/health", (_req, res) => {
+    return res.status(200).json({
+        success: true,
+        message: "Service is running successfully. Please use the following endpoint to access the API:"
+    });
+});
+
+
 app.use("/polls", pollRoutes)
+
+app.use((req, res) => {
+    res.status(404).json({
+        error: "Route not found"
+    })
+})
 
 // GLOBAL ERROR HANDLER
 app.use(errorMiddleware)
