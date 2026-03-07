30b318af711421e7ebe9b12f08bbb0bf969717b0
xaltyPasta <rajkumarravi9470@gmail.com>
2026-03-07 14:01:19 +0530

    initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import LandingPage from "./pages/LandingPage"
+import HistoryPage from "./pages/PollHistoryPage"
+import KickedPage from "./pages/KickedPage"
+import StudentJoinPage from "./pages/StudentJoinPage"
+import StudentWaitingPage from "./pages/StudentWaitingPage"
+import StudentVotePage from "./pages/StudentVotePage"
+import TeacherCreatePollPage from "./pages/TeacherCreatePollPage"
+import TeacherLiveResultsPage from "./pages/TeacherLiveResultsPage"
+import StudentResultPage from "./pages/StudentReesultPage"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/history" element={<HistoryPage />} />
+        <Route path="/student" element={<StudentJoinPage />} />
+        <Route path="/kicked" element={<KickedPage />} />
+        <Route path="/student/result" element={<StudentResultPage />} />
+        <Route path="/student/vote" element={<StudentVotePage />} />
+        <Route path="/student/wait" element={<StudentWaitingPage />} />
+        <Route path="/teacher" element={<TeacherCreatePollPage />} />
+        <Route path="/teacher/live" element={<TeacherLiveResultsPage />} />
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
