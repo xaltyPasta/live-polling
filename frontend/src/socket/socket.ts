36391ce79af385b088cd4f8445fa55aad32fa4f8
xaltyPasta <rajkumarravi9470@gmail.com>
2026-03-11 00:59:17 +0530

    env var updated

--- frontend/src/socket/socket.ts
+++ frontend/src/socket/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client"
 
-const BACKEND_URL = import.meta.env.BACKEND_URL || "http://localhost:5000"
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000"
 
 export const socket: Socket = io(BACKEND_URL, {
     transports: ["websocket"],
